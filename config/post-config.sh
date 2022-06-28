c2ae1ed483c71722f58bf6c3d9da05a1b771d57c
DanielFroehlich <daniel.froehlich@redhat.com>
2022-06-28 12:36:52 +0200

    Adding heap dump enablement. Removing sample account addition

--- config/post-config.sh
+++ config/post-config.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-echo "############## Example Custom Script ###############"
-echo "##                                                ##"
-echo "##      This is an example configure script.      ##"
-echo "##                                                ##"
-echo "####################################################"
+echo "############# Custom Config Script ###############"
+echo "##                                              ##"
+echo "##      This is a custom configure script.      ##"
+echo "##                                              ##"
+echo "##################################################"
 
 echo "#### The config dir locates at ${CONFIG_INSTANCE_DIR} ####"
-ls ${CONFIG_INSTANCE_DIR}
-echo Adding a new account "test" with password "test"
-printf '\n' >> ${CONFIG_INSTANCE_DIR}/etc/artemis-users.properties
-echo "test = test" >> ${CONFIG_INSTANCE_DIR}/etc/artemis-users.properties
-# visitor has role 'viewer'
-# /etc/artemis-roles.properties
-echo "adding test role as viewer"
-printf '\n' >> "${CONFIG_INSTANCE_DIR}"/etc/artemis-roles.properties
-echo "viewer = test" >> ${CONFIG_INSTANCE_DIR}/etc/artemis-roles.properties
-## Add role 'viewer' to HAWTIO_ROLE so that it can login to the console
-sed -i "s/hawtio.role=admin/hawtio.role=admin,viewer/g" ${CONFIG_INSTANCE_DIR}/etc/artemis.profile
+
+echo "Enabling Heap Dump"
+echo '' >>"${CONFIG_INSTANCE_DIR}"/etc/artemis.profile
+echo '# Enabling dumps' >>"${CONFIG_INSTANCE_DIR}"/etc/artemis.profile
+echo 'JAVA_ARGS="$JAVA_ARGS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/amq-broker/data"' >>"${CONFIG_INSTANCE_DIR}"/etc/artemis.profile
+echo 'JAVA_ARGS="$JAVA_ARGS $JAVA_EXTRA_ARGS"' >>"${CONFIG_INSTANCE_DIR}"/etc/artemis.profile
 
 
-echo "### Adding wildcard config to broker config"
+echo "Adding wildcard config to broker config"
 # this searches for the </core> tag in the config at replaces
 # it with the content of the wildcard.xml file
 sed -e '/\/core/r /amq/scripts/custom-config.xml' -e '/\/core/d' ${CONFIG_INSTANCE_DIR}/etc/broker.xml >${CONFIG_INSTANCE_DIR}/etc/broker_new.xml
 
-echo "### Copying broker_new.xml to  ${CONFIG_INSTANCE_DIR}/etc/broker.xml"
+echo "Copying broker_new.xml to  ${CONFIG_INSTANCE_DIR}/etc/broker.xml"
 cp ${CONFIG_INSTANCE_DIR}/etc/broker_new.xml ${CONFIG_INSTANCE_DIR}/etc/broker.xml
 
 
